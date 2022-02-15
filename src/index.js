8f225b39d7e63a36784658e1fc3a1af6377207fb
sboursen <dev.sboursen@gmail.com>
2022-02-15 18:01:41 +0100

    Add to do tasks list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const toDoTasks = [
+  {
+    description: 'Play guitar',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Eat ice cream',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Watch a movie',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Hangout with friends',
+    completed: false,
+    index: 3,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+const toDoList = document.querySelector('to-do-list');
+const displayTasks = (e) => {
+  if (e.currentTarget.classList.contains('to-do-list')) {
+    toDoTasks.forEach((task) => {
+      console.log(task);
+    });
+  }
+};
+toDoList.addEventListener('DOMContentLoaded', displayTasks);
