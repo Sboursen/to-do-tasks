736d4ba1f96b933eaece1b84896fca0635293889
sboursen <dev.sboursen@gmail.com>
2022-02-15 23:03:50 +0100

    Complete the style overhaul to nier ui

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
