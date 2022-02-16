6249c1e3d02dbd9067bb5b5a32a047c96aeb7039
sboursen <dev.sboursen@gmail.com>
2022-02-16 10:03:44 +0100

    Create a DOM elements Module

--- src/index.js
+++ src/index.js
@@ -1,56 +1,7 @@
 import './style.css';
 import Task from './components/task';
+import displayTasks, * as crud from './components/crud-operations';
 
 let myTask = new Task('Play guitar', false, 67);
 
-
-const toDoTasks = [
-  {
-    description: 'Play guitar',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Eat ice cream',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Watch a movie',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Hangout with friends',
-    completed: false,
-    index: 3,
-  },
-];
-
-const toDoList = document.querySelector('.to-do-list');
-toDoList.style.gridTemplateRows = `repeat(${
-  Object.keys(toDoTasks).length
-}, 48px);`;
-
-const displayTasks = () => {
-  toDoTasks
-    .sort((obj1, obj2) => obj1.index - obj2.index)
-    .forEach((task) => {
-      const li = `<li class="task">
-                <input type="checkbox" name="check-${
-                  task.index
-                }" id="${task.index}" ${
-        task.completed ? 'checked' : null
-      }>
-                <span class="description">${
-                  task.description
-                }</span>
-                <button type="button" class="icon">
-                  <i class="fa-solid fa-ellipsis-vertical"></i>
-                </button>
-              </li>`;
-
-      toDoList.innerHTML += `\n ${li}`;
-    });
-};
 window.addEventListener('DOMContentLoaded', displayTasks);
