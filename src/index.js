9fc36669f33aba544d1e79b7ebb99c7c78992bba
sboursen <dev.sboursen@gmail.com>
2022-02-17 19:05:15 +0100

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './style.css';
 import CRUD from './components/crud-operations';
+
 const crud = new CRUD();
 
 window.addEventListener(
   'DOMContentLoaded',
   crud.initializeApplication,
 );
-
-
