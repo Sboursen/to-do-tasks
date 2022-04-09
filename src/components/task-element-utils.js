bf1d06d5355cb481cb357209b054e8802b5062cc
sboursen <dev.sboursen@gmail.com>
2022-04-09 11:56:06 +0100

    Fix strickthrough discription bug

--- src/components/task-element-utils.js
+++ src/components/task-element-utils.js
@@ -132,27 +132,15 @@ function addInputEvents(taskElement) {
 
 export default function createTaskElement(task) {
   const tmpWrapper = document.createElement('div');
-  const taskStringElement = `<li class="task" id="task-${
-    task.index
-  }">
-        <input type="checkbox" name="check-${
-  task.index
-}" id="check-${task.index}" ${
-  task.completed ? 'checked' : null
-}>
+  const taskStringElement = `<li class="task" id="task-${task.index}">
+        <input type="checkbox" name="check-${task.index}" id="check-${task.index}" ${task.completed ? 'checked' : null}>
         <label for="description" class="description">
-          <input type="text" name="description" value="${
-  task.description
-}" id="description-${task.index}">
+          <input type="text" name="description" value="${task.description}" id="description-${task.index}">
         </label>
-        <button type="button" class="icon move-button" id="moveButton-${
-  task.index
-}">
+        <button type="button" class="icon move-button" id="moveButton-${task.index}">
           <i class="fa-solid fa-ellipsis-vertical"></i>
         </button>
-        <button type="button" class="icon delete-button" id="deleteButton-${
-  task.index
-}">
+        <button type="button" class="icon delete-button" id="deleteButton-${task.index}">
           <i class="fas fa-trash-alt"></i>
         </button>
       </li>`;
